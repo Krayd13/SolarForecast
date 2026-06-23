1ea7d9ec3a3e5be199613329d26a8cf27f2dad97
Krayd13 <danik13112009@gmail.com>
2026-06-23 19:16:01 +0300

    update

--- src/main/java/com/example/solar/provider/ActualProvider.java
+++ src/main/java/com/example/solar/provider/ActualProvider.java
@@ -3,6 +3,7 @@ package com.example.solar.provider;
 import com.example.solar.SourceNames;
 import com.example.solar.model.ForecastData;
 import com.example.solar.model.Station;
+import com.example.solar.model.StationPanel;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -19,13 +20,11 @@ public class ActualProvider implements ForecastProvider{
     @Override
     public List<ForecastData> fetch(Station station) {
         List<ForecastData> testData = new ArrayList<>();
-        // Беремо початок сьогоднішнього дня (00:00)
         LocalDateTime todayStart = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
 
         for (int hour = 0; hour < 24; hour++) {
             LocalDateTime timestamp = todayStart.withHour(hour);
 
-            // Чиста синусоїда: пік генерації вдень, вночі (до 6 ранку і після 18) - по нулях
             double baseValue = 5.0 * Math.sin(Math.PI * hour / 24.0);
             if (hour < 5 || hour > 19) {
                 baseValue = 0.0;
@@ -35,9 +34,14 @@ public class ActualProvider implements ForecastProvider{
                     .station(station)
                     .sourceName(getSourceName())
                     .timestamp(timestamp)
-                    .value(baseValue) // Ідеальний факт без шуму
+                    .value(baseValue)
                     .build());
         }
         return testData;
+    }
+
+    @Override
+    public String buildUrlForPanel(Station station, StationPanel panel) {
+        return null;
     }
 }
